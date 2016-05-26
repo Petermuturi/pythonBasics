1768476816b9978df3b3a4551679e811116ef055
Petermuturi <pmuturi65@gmail.com>
2016-05-26 10:40:07 +0300

    whileLoop -magicNumber

---
+++ whileLoop.py
@@ -0,0 +1,8 @@
+magicNumber = 14
+luckyNumber = 0
+
+while luckyNumber != magicNumber:
+    print(luckyNumber, 'not the lucky number')
+    luckyNumber += 1
+
+print(luckyNumber, "is the luck number:D \n we'll stop at that:)")
