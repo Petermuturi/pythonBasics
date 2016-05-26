ca27cc94cb8108b7957049269211922c7790674f
Petermuturi <pmuturi65@gmail.com>
2016-05-26 10:31:30 +0300

    forLoop -ages

---
+++ forLoop.py
@@ -0,0 +1,9 @@
+ageList = [32, 21, 56, 44]
+
+for ages in ageList:
+    if ages < 30:
+        print(ages, 'sorry, too young for the club')
+    elif ages >50:
+        print(ages, 'sorry, too elderly for the club')
+    else:
+        print(ages, 'Welcome to the club')
